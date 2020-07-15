590f5be8023fdc496187af0fda759a028213a3c4
Angelo Aracri <angeloaracri@yahoo.de>
2020-07-15 14:45:02 +0200

    Connected Persistence with audioSettings

--- scenes/ui/menu/MenuSlider.gd
+++ scenes/ui/menu/MenuSlider.gd
@@ -11,20 +11,36 @@ onready var label:Label = $Label
 onready var slider:HSlider = $HSlider
 
 #############################################################
+# STATE
+var valueOrPersistenceUIDPath
+
+#############################################################
 # LIFECYCLE
-func init(caption, value, value_min, value_max, value_step):
+func init(caption, valueOrPersistenceUIDPath, value_min, value_max, value_step):
 	if !label: label = $Label
 	if !slider: slider = $HSlider
 	
 	label.text = caption
+	self.valueOrPersistenceUIDPath = valueOrPersistenceUIDPath
 	
-	slider.value = int(value)
+	if _is_synced_with_persistence():
+		slider.value = PersistenceManager.get_val_from_ui_path(valueOrPersistenceUIDPath)
+	else:
+		slider.value = int(valueOrPersistenceUIDPath)
+	
 	slider.min_value = int(value_min)
 	slider.max_value = int(value_max)
 	slider.step = int(value_step)
 	return self
 
+func _is_synced_with_persistence():
+	return valueOrPersistenceUIDPath is String and not valueOrPersistenceUIDPath.is_valid_integer()
 #############################################################
 # CALLBACKS	
 func _on_HSlider_value_changed(value):
+	
+	# Update PersistentObject if synced to it
+	if _is_synced_with_persistence():
+		PersistenceManager.set_val_from_ui_path(valueOrPersistenceUIDPath, value)
+	
 	emit_signal("slider_changed", slider.value)
--- scripts/globals/State.gd
+++ scripts/globals/State.gd
@@ -1,1 +1,11 @@
+tool
 extends Node
+
+
+
+func _ready():
+	PersistenceManager.add_obj(PersistenceManager.PersistentObj.new("settingsAudio", {
+		"Master" : 80,
+		"Music" : 100,
+		"Effects" : 100
+	}))
