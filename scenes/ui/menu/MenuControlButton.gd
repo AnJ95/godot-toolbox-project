e9b926c628be7b58d65ff27cc5e3c1fdf1dfcf27
Angelo Aracri <angeloaracri@yahoo.de>
2020-07-15 16:59:10 +0200

    Made MenuControlButton select a keycode

--- scenes/ui/menu/MenuControlButton.gd
+++ scenes/ui/menu/MenuControlButton.gd
@@ -19,6 +19,7 @@ export(String) var input_action_name = "none"
 #############################################################
 # STATE
 var scancode
+var awaiting = false
 
 #############################################################
 # LIFECYCLE
@@ -31,5 +32,56 @@ func _ready():
 	return self
 
 #############################################################
-# CALLBACKS	
+# AWAITING CONDITION
+func start_awaiting():
+	for control_button in get_all_menu_control_buttons():
+		control_button.end_awaiting()
+	button.pressed = true
+	awaiting = true
+	button.text = "<Enter Key>"
+	
+func end_awaiting():
+	awaiting = false
+	button.pressed = false
+	button.text = get_display_caption()
 
+#############################################################
+# GETTERS & SETTERS
+func set_scan_code(new_scancode):
+	# Set locally
+	scancode = new_scancode
+	
+	# Update PersistentObj
+	PersistenceManager.set_val_from_ui_path(persistence_uid_path, scancode)
+	
+	# Unassign all other that have the same key
+	for control_button in get_all_menu_control_buttons():
+		if control_button != self and control_button.scancode == scancode:
+			control_button.set_scan_code(null)
+	
+	# Update button text
+	button.text = get_display_caption()
+	
+func get_display_caption():
+	if scancode:
+		return OS.get_scancode_string(scancode)
+	else:
+		return "Unassigned"
+
+func get_all_menu_control_buttons():
+	return get_tree().get_nodes_in_group("MenuControlButton")
+	 
+#############################################################
+# CALLBACKS	
+func _on_Button_pressed():
+	if button.pressed:
+		start_awaiting()
+	else:
+		end_awaiting()
+
+func _input(event):
+	if awaiting and event is InputEventKey:
+		var event_key:InputEventKey = event
+		if event_key.pressed:
+			set_scan_code(event_key.scancode)
+			end_awaiting()
