b28fc44e555e2de577c9b8e588a745c6c9987a2d
Angelo Aracri <angeloaracri@yahoo.de>
2020-07-17 17:16:15 +0200

    Added ui components and built generic GameUI

---
+++ scenes/ui/components/IconIntValue.gd
@@ -0,0 +1,15 @@
+extends HBoxContainer
+
+export var trailing_zeroes = 2
+
+onready var label = $Label
+
+func _on_update_int(int_text:int):
+	var str_text = str(int_text)
+	
+	# add trailing 0s if configured
+	while str_text.size() < trailing_zeroes:
+		str_text = "0" + str_text
+		
+	label.text = str_text
+	
---
+++ scenes/ui/components/IconValue.gd
@@ -0,0 +1,54 @@
+tool
+extends HBoxContainer
+
+export var has_halfes = true
+export var value_now = 1.5
+export var value_max = 3
+
+export(Texture) var texture
+
+var region_width
+var region_height
+
+func _ready():
+	region_width = texture.get_width() / 3
+	region_height = texture.get_height()
+	
+	init_value()
+	update_value()
+
+func init_value():
+	for icon in get_children():
+		icon.queue_free()
+	
+	for i in range(value_max):
+		var atlas:AtlasTexture = AtlasTexture.new()
+		atlas.atlas = texture
+		atlas.region = Rect2(2 * region_width, 0, region_width, region_height)
+		
+		var icon:TextureRect = TextureRect.new()
+		#icon.expand = true
+		icon.stretch_mode = icon.STRETCH_KEEP_ASPECT_CENTERED
+		icon.size_flags_vertical = icon.SIZE_EXPAND_FILL
+		icon.size_flags_horizontal = icon.SIZE_EXPAND_FILL
+		icon.anchor_right = 1
+		icon.anchor_bottom = 1
+
+		icon.texture = atlas
+		add_child(icon)
+
+func update_value():
+	var value_left = value_now
+
+	for icon in get_children():
+		
+		var region_id = 2
+		if value_left >= 1:
+			value_left -= 1
+			region_id = 0
+		elif has_halfes and value_left >= 0.5:
+			value_left -= 0.5
+			region_id = 1
+			
+		icon.texture.region.position.x = region_id * region_width
+		
