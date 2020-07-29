dff1df788029482d52d6e9fa892c9626e5dd366b
Angelo Aracri <angeloaracri@yahoo.de>
2020-07-29 17:01:31 +0200

    Added CheckBox to theme

--- scenes/ui/ThemeTester.gd
+++ scenes/ui/ThemeTester.gd
@@ -2,9 +2,11 @@ tool
 extends GridContainer
 
 func _ready():
-	#$VBoxContainer7/ConfirmationDialog.visible = true
-	$VBoxContainer7/ConfirmationDialog.margin_top = 50
+	$VBoxContainer7/ConfirmationDialog.visible = true
+	#$VBoxContainer7/ConfirmationDialog.margin_top = 50
 	
-	$VBoxContainer8/OptionButton.add_item("High", 0)
-	$VBoxContainer8/OptionButton.add_item("Medium", 1)
-	$VBoxContainer8/OptionButton.add_item("Low", 2)
+	var btn_option:OptionButton = $VBoxContainer8/OptionButton
+	btn_option.clear()
+	btn_option.add_item("High", 0)
+	btn_option.add_item("Medium", 1)
+	btn_option.add_item("Low", 2)
